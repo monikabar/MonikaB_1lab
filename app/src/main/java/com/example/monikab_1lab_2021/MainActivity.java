a9c47d312a067e89f332cf14199799c2a435ce1a
monikabar <monikabarkauskaite@gmail.com>
2021-04-19 00:27:09 +0300

    Added change text functionality

--- app/src/main/java/com/example/monikab_1lab_2021/MainActivity.java
+++ app/src/main/java/com/example/monikab_1lab_2021/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.monikab_1lab_2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void OnBtnClick(View view) {
+        this.tvMain.setText(R.string.changed_label_caption);
     }
 }
