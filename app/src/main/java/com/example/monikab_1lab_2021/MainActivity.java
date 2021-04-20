7c97f4c7ddbf3a99cc227fa668d9eddcebdac389
monikabar <monikabarkauskaite@gmail.com>
2021-04-20 20:36:13 +0300

    Pridetas antras mygtukas

--- app/src/main/java/com/example/monikab_1lab_2021/MainActivity.java
+++ app/src/main/java/com/example/monikab_1lab_2021/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.monikab_1lab_2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void OnBtnClick(View view) {
         this.tvMain.setText(R.string.changed_label_caption);
+    }
+
+    public void OnBtnClick2(View view) {
+        this.tvMain.setTextColor(Color.RED);
     }
 }
